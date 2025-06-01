1653c733617eb8778e5e661dbf6994f36162dae2
Rohit Raj <144614413+gh-rohit@users.noreply.github.com>
2025-06-02 05:05:36 +0530

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "https://personal-travel-diary-app.onrender.com/api"
+const BASE_URL = "https://travel-tales-backend-gegu.onrender.com/api"
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
